836ffebcefb04c6e86fef220a901912a2ade25ae
Mieszko Pietrzak <pietrzakmieszko@gmail.com>
2023-04-14 10:18:26 +0200

    Sign up form added

--- witch-blog-web-frontend/src/components/SignUp.js
+++ witch-blog-web-frontend/src/components/SignUp.js
@@ -8,7 +8,7 @@ const RE_PASSWD = /^\S.{8,}$/
 // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
 const RE_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-const Form = props => {
+const SignUp = props => {
   const { register, formState: { errors }, handleSubmit, getValues } = useForm()
   const onSubmit = (data) => console.log(data)
 
@@ -67,4 +67,4 @@ const Form = props => {
   )
 }
 
-export default Form
+export default SignUp
--- witch-blog-web-frontend/src/index.js
+++ witch-blog-web-frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Form from "./components/SignUp"
+import SignUp from "./components/SignUp"
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Form/>
+    <SignUp/>
   </React.StrictMode>
 );
 
