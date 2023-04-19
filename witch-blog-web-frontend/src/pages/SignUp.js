b07d2380c75225ef3681cc206bd492c0c16d0701
Mieszko Pietrzak <pietrzakmieszko@gmail.com>
2023-04-19 18:00:00 +0200

    on submit func

--- witch-blog-web-frontend/src/pages/SignUp.js
+++ witch-blog-web-frontend/src/pages/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm } from "react-hook-form"
+import axios from 'axios'
 import GoogleLoginButton from "../components/GoogleLoginButton"
 import './SignUp.css'
 
@@ -10,21 +11,42 @@ const RE_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((
 
 const SignUp = props => {
   const { register, formState: { errors }, handleSubmit, getValues } = useForm()
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => {
+    delete data.password2
+    console.log(JSON.stringify(data))
+     axios
+       .post('https://witchblog.azurewebsites.net/api/v1/auth/signup', data)
+       .then((res) => {
+         console.log(res)
+       })
+       .catch((err) => {
+          console.log(err)
+       })
+  }
 
   return (
     <div className="wrap">
       <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Sign up!</h1>
         <div className="form-item">
-            <h2>Username</h2>
+            <h2>First Name</h2>
             <input
-            {...register("username", {
+            {...register("firstName", {
               required: true,
               pattern: { value: RE_USER }
             })}
           />
-          {errors.username && <p className="error-txt">Wymagane min. 3 znaki, brak spacji</p>}
+          {errors.firstName && <p className="error-txt">Wymagane min. 3 znaki, brak spacji</p>}
+        </div>
+        <div className="form-item">
+            <h2>Last Name</h2>
+            <input
+            {...register("lastName", {
+              required: true,
+              pattern: { value: RE_USER }
+            })}
+          />
+          {errors.lastName && <p className="error-txt">Wymagane min. 3 znaki, brak spacji</p>}
         </div>
         <div className="form-item">
           <h2>Email Address</h2>
@@ -39,26 +61,26 @@ const SignUp = props => {
         <div className="form-item">
             <h2>Password</h2>
             <input type="password"
-            {...register("passwd", {
+            {...register("password", {
               required: true,
               pattern: { value: RE_PASSWD }
             })}
           />
-          {errors.passwd && <p className="error-txt">Wymagane min. 8 znaków, brak spacji</p>}
+          {errors.password && <p className="error-txt">Wymagane min. 8 znaków, brak spacji</p>}
         </div>
         <div className="form-item">
             <h2>Repeat password</h2>
             <input type="password"
-            {...register("passwd2", {
+            {...register("password2", {
               required: true,
               validate: (val) => {
-                if (getValues('passwd') !== val) {
+                if (getValues('password') !== val) {
                   return "Your passwords do no match";
                 }
               },
             })}
           />
-          {errors.passwd2 && <p className="error-txt">Hasła do siebie nie pasują</p>}
+          {errors.password2 && <p className="error-txt">Hasła do siebie nie pasują</p>}
         </div>
         <input className="send-btn" type="submit" />
         <GoogleLoginButton />
