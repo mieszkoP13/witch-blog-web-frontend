0c35f08ed645ac6815ea71b4affbc0a5d5e3e6d7
kgawerda <konradgawerda@gmail.com>
2023-04-27 20:45:02 +0200

    fix facebook button

--- witch-blog-web-frontend/src/components/FacebookLoginButton.js
+++ witch-blog-web-frontend/src/components/FacebookLoginButton.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./FacebookLoginButton.css";
 
-function GoogleLoginButton() {
+function FacebookLoginButton() {
   return (
     <a href="https://witchblog.azurewebsites.net/oauth2/authorize/facebook?redirect_uri=http://localhost:3000/users/profile">
-      <button className="btn-google">Sign in with Facebook </button>
+      <button className="btn-facebook">Sign in with Facebook </button>
     </a>
   );
 }
 
-export default GoogleLoginButton;
+export default FacebookLoginButton;
