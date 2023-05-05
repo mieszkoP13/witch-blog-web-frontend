f7207283b85566748139e2ff6760baedab2fcdc4
Mieszko Pietrzak <pietrzakmieszko@gmail.com>
2023-05-05 22:32:37 +0200

    page is almost finished

--- witch-blog-web-frontend/src/components/Card.js
+++ witch-blog-web-frontend/src/components/Card.js
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react'
 import Back from './Back.svg';
 import "./Card.css"
 
-const Card = props => {
+const Card = ({base64,updateFlip,id}) => {
   const [flip,setFlip] = useState(false)
 
   const flipOnce = () => setFlip(true)
 
+  useEffect(() => {
+    updateFlip(flip,id)
+  }, [flip])
+
   return (
     <div onClick={flipOnce} className="card-div">
-      <img className="card" src={flip ? `data:image/jpeg;base64,${props.base64}` : Back} alt="Card" />
+      <img className="card" src={flip ? `data:image/jpeg;base64,${base64}` : Back} alt="Card" />
     </div>
   )
 }
