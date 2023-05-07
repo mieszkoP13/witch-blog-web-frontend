ba71988cac5c6dd354ffc2c03ce0f710c3ad37f9
kgawerda <konradgawerda@gmail.com>
2023-05-07 12:20:55 +0200

    change folder structure

---
+++ src/components/PopUp.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import "./PopUp.css";
+
+const PopUp = (props) => {
+  useEffect(() => {
+    const clickOutside = (e) => {
+      if (!document.querySelector(".popup").contains(e.target))
+        props.setShow(false);
+    };
+    window.addEventListener("click", clickOutside);
+    return () => window.removeEventListener("click", clickOutside);
+  }, [props]);
+
+  return (
+    <div className={`popup ${props.show ? null : "hide"}`}>
+      {props.children}
+      <button className="btn-popup" onClick={() => props.setShow(false)}>
+        Ok
+      </button>
+    </div>
+  );
+};
+
+export default PopUp;
