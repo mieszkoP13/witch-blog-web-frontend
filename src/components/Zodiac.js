8a72b4eb594b6101a4ca43e75b8abb5e2e64e60d
Mieszko Pietrzak <pietrzakmieszko@gmail.com>
2023-05-19 20:37:44 +0200

    zodiac signs path fix 3

--- src/components/Zodiac.js
+++ src/components/Zodiac.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import "./Zodiac.css"
-import Aquarius from './src/components/zodiac-signs-assets/Aquarius.jpg';
-import Aries from './src/components/zodiac-signs-assets/Aries.jpg';
-import Cancer from './src/components/zodiac-signs-assets/Cancer.jpg';
-import Capricorn from './src/components/zodiac-signs-assets/Capricorn.jpg';
-import Gemini from './src/components/zodiac-signs-assets/Gemini.jpg';
-import Leo from './src/components/zodiac-signs-assets/Leo.jpg';
-import Libra from './src/components/zodiac-signs-assets/Libra.jpg';
-import Pisces from './src/components/zodiac-signs-assets/Pisces.jpg';
-import Sagittarius from './src/components/zodiac-signs-assets/Sagittarius.jpg';
-import Scorpio from './src/components/zodiac-signs-assets/Scorpio.jpg';
-import Taurus from './src/components/zodiac-signs-assets/Taurus.jpg';
-import Virgo from './src/components/zodiac-signs-assets/Virgo.jpg';
+import Aquarius from '../zodiac-signs-assets/Aquarius.jpg';
+import Aries from '../zodiac-signs-assets/Aries.jpg';
+import Cancer from '../zodiac-signs-assets/Cancer.jpg';
+import Capricorn from '../zodiac-signs-assets/Capricorn.jpg';
+import Gemini from '../zodiac-signs-assets/Gemini.jpg';
+import Leo from '../zodiac-signs-assets/Leo.jpg';
+import Libra from '../zodiac-signs-assets/Libra.jpg';
+import Pisces from '../zodiac-signs-assets/Pisces.jpg';
+import Sagittarius from '../zodiac-signs-assets/Sagittarius.jpg';
+import Scorpio from '../zodiac-signs-assets/Scorpio.jpg';
+import Taurus from '../zodiac-signs-assets/Taurus.jpg';
+import Virgo from '../zodiac-signs-assets/Virgo.jpg';
 const ZODIAC_IMGS = [Aquarius, Aries, Cancer, Capricorn, Gemini, Leo, Libra, Pisces, Sagittarius, Scorpio, Taurus, Virgo]
 
 const Zodiac = ({id,updateCheck}) => {
