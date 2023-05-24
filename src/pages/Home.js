7e4e1f47340a524685ce79ff98870c7d71503742
kgawerda <konradgawerda@gmail.com>
2023-05-24 15:14:45 +0200

    add home page and fix animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Home.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <></>;
+  const [active, setActive] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/Divinations");
+  };
+  return (
+    <>
+      <div
+        onClick={handleClick}
+        onMouseEnter={() => setActive(true)}
+        onMouseLeave={() => setActive(false)}
+        className={active ? "center active" : "center"}
+      ></div>
+    </>
+  );
 };
 
 export default Home;
