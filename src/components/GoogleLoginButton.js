1fc61416bc6f737fa0c0118174468374f54e6ed6
kgawerda <konradgawerda@gmail.com>
2023-06-08 19:52:19 +0200

    fix unnecessary change in google link

--- src/components/GoogleLoginButton.js
+++ src/components/GoogleLoginButton.js
@@ -4,7 +4,7 @@ import googleLogo from "../imgs/light/logo-google.svg";
 
 function GoogleLoginButton() {
   return (
-    <a href="https://witchblog.azurewebsites.net/oauth2/authorize/google?redirect-uri=http://localhost:3000/users/profile">
+    <a href="https://witchblog.azurewebsites.net/oauth2/authorize/google?redirect_uri=http://localhost:3000/users/profile">
       <div className="button">
         <img className="google-logo" src={googleLogo} alt="google logo" />
         Sign in with Google
